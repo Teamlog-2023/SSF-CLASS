2c43bafe3a0c80e711c6eb13af23a8ce1a51793e
theta77 <springjumpyt@gmail.com>
2023-08-16 18:59:59 +0900

    함수 예제

---
+++ SSF-CLASS-master/teacher/3.JS(심화)/2,함수/examples.js
@@ -0,0 +1,70 @@
+// function hello() {
+//     console.log("Hello World");
+// }
+
+
+// hello();
+
+
+
+
+// function number(a){
+// 	console.log(a);
+// }
+
+// let age = 18;
+
+
+
+// number(age);
+
+
+
+// function plus(a,b) {
+// 	console.log(`plus : ${a + b}`);
+// }
+
+
+
+// plus(5,5);
+
+
+
+// function plus(a,b) {
+//     return a + b;
+// }
+
+// let plus_num = plus(5,5);
+
+
+
+// console.log(`plus : ${plus_num}`);
+
+
+
+
+function plus(a,b) {
+    return a + b;
+}
+function minus(a,b) {
+    return a - b;
+}
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
+
+let plus_num = plus(5,5);
+let minus_num = minus(5,5);
+let multiply_num = multiply(5,5);
+let divide_num = divide(5,5);
+
+
+
+console.log(`plus : ${plus_num}`);
+console.log(`minus : ${minus_num}`);
+console.log(`multiply : ${multiply_num}`);
+console.log(`divide : ${divide_num}`);

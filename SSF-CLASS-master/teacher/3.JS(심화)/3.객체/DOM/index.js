3047d819ce67a6d44869e8aea1ca3c9c0701da7c
theta77 <springjumpyt@gmail.com>
2023-08-19 19:52:49 +0900

    예제

--- SSF-CLASS-master/teacher/3.JS(심화)/3.객체/DOM/index.js
+++ SSF-CLASS-master/teacher/3.JS(심화)/3.객체/DOM/index.js
@@ -1,9 +1,43 @@
 // console.log(document);
 
 // const p = document.querySelector('p');
-
 // console.log(p)
 
-const p = document.getElementById('p');
+// const p = document.getElementById('p');
+// console.log(p)
 
-console.log(p)
+// 속성 관련 메소드
+// let p = document.createElement("p");
+// document.body.appendChild(p);
+
+// 속성 관련 프로퍼티
+// let p = document.createElement("p");
+// p.className = "teamlog";
+// p.innerHTML = "Hello World";
+// document.body.appendChild(p);
+
+var count = 1;
+function club() {
+    let club = document.querySelector('h1');
+    if (count == 1) {
+        club.innerText = "Layer7";
+    }
+    else if (count == 2) {
+        club.innerText = "Emotion";
+    }
+    else if (count == 3) {
+        club.innerText = "Unifox";
+    }
+    else if (count == 4) {
+        club.innerText = "Nefus";
+    }
+    else {
+        club.innerText = "Team Log";
+    }
+    if (count != 5) {
+        count += 1;
+    }
+    else {
+        count = 0;
+    }
+}
